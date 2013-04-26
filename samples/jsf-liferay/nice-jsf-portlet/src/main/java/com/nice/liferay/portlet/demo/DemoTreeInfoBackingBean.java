f911419e0664112d9eb144fe623fe36c82740f22
grantking <gking.uk@gmail.com>
2013-04-26 13:03:28 -0700

    Added tree demo

--- samples/jsf-liferay/nice-jsf-portlet/src/main/java/com/nice/liferay/portlet/demo/DemoTreeInfoBackingBean.java
+++ samples/jsf-liferay/nice-jsf-portlet/src/main/java/com/nice/liferay/portlet/demo/DemoTreeInfoBackingBean.java
@@ -6,4 +6,26 @@ import org.springframework.stereotype.Component;
 public class DemoTreeInfoBackingBean {
 	
 	
+	private String treeName;
+	private String treeDescription;
+	
+	public String getTreeName() {
+		return treeName;
+	}
+	public void setTreeName(String treeName) {
+		this.treeName = treeName;
+	}
+	public String getTreeDescription() {
+		return treeDescription;
+	}
+	public void setTreeDescription(String treeDescription) {
+		this.treeDescription = treeDescription;
+	}
+	
+	
+	public void saveTree(){
+		System.out.println(treeName + " - "+ treeDescription);
+	}
+	
+	
 }
